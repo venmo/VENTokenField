17255a3f174fab8a9d1a862c20a5991ac3740b39
Sheng Dong <sheng@fuzzproductions.com>
2014-11-07 17:17:25 -0500

    initialize autocorrectionType

--- VENTokenField/VENTokenField.m
+++ VENTokenField/VENTokenField.m
@@ -79,6 +79,7 @@ static const CGFloat VENTokenFieldDefaultMaxHeight          = 150.0;
 - (void)setUpInit
 {
     // Set up default values.
+    _textAutocorrectionType = UITextAutocorrectionTypeNo;
     self.maxHeight = VENTokenFieldDefaultMaxHeight;
     self.verticalInset = VENTokenFieldDefaultVerticalInset;
     self.horizontalInset = VENTokenFieldDefaultHorizontalInset;
@@ -346,7 +347,7 @@ static const CGFloat VENTokenFieldDefaultMaxHeight          = 150.0;
         _inputTextField.textColor = self.inputTextFieldTextColor;
         _inputTextField.font = [UIFont fontWithName:@"HelveticaNeue" size:15.5];
         _inputTextField.accessibilityLabel = NSLocalizedString(@"To", nil);
-        _inputTextField.autocorrectionType = UITextAutocorrectionTypeNo;
+        _inputTextField.autocorrectionType = self.textAutocorrectionType;
         _inputTextField.tintColor = self.colorScheme;
         _inputTextField.delegate = self;
         _inputTextField.placeholder = self.placeholderText;
