1dc074ac11b42009fd4c7a918667b188ccbc11f3
Mark Adams <mark.adams@venmo.com>
2015-09-30 10:28:31 -0400

    Add nullability specifiers

--- VENTokenField/VENBackspaceTextField.h
+++ VENTokenField/VENBackspaceTextField.h
@@ -22,6 +22,8 @@
 
 #import <UIKit/UIKit.h>
 
+NS_ASSUME_NONNULL_BEGIN
+
 @class VENBackspaceTextField;
 
 @protocol VENBackspaceTextFieldDelegate <UITextFieldDelegate>
@@ -34,3 +36,5 @@
 @property (weak, nonatomic) id<VENBackspaceTextFieldDelegate> backspaceDelegate;
 
 @end
+
+NS_ASSUME_NONNULL_END
